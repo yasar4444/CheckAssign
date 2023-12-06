5d3659040f37a7120e8ab71f77ac0801092dc52d
yasar4444 <147474615+yasar4444@users.noreply.github.com>
2023-12-06 07:24:29 +0530

    Add files via upload

---
+++ messenger.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Messenger</title>
+</head>
+<body>
+    <h1 styles ="background-color:rgb(255,180,30);">Messenger</h1>
+    <br><br>
+    
+    <b>Enter recipient account</b>
+    <form>
+        <input type ='text' value='' placeholder='Account ID...'/>
+    </form>
+    <br>
+    <form>
+        <input type ='text' size='100' value='' placeholder='Message...'/>
+    </form>
+    <br>
+    <button type="button">Send</button>
+    </body>
+    </html>
